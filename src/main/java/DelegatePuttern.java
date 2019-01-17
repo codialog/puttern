1f4d916dc9d2246077c2b0ad2f8dd490030e63f4
Vladimir Druzhinin <dialog.txt@gmail.com>
2019-01-17 21:00:49 +0700

    Added delegate puttern [codialog]

---
+++ src/main/java/DelegatePuttern.java
@@ -0,0 +1,40 @@
+public class DelegatePuttern {
+    public static void main(String[] args) {
+        Painter painter = new Painter();
+        painter.setGraphics(new Square());
+        painter.drow();
+    }
+}
+
+interface Graphics {
+    void draw();
+}
+
+class Triangle implements Graphics {
+    public void draw() {
+        System.out.println("triangle");
+    }
+}
+
+class Square implements Graphics {
+    public void draw() {
+        System.out.println("square");
+    }
+}
+
+class Circle implements Graphics {
+    public void draw() {
+        System.out.println("circle");
+    }
+}
+class Painter {
+    Graphics graphics;
+    void setGraphics(Graphics g)  {
+        graphics = g;
+    }
+
+    void drow() {
+        // делегируется метод ресованеие, фигуре
+        graphics.draw();
+    }
+}
